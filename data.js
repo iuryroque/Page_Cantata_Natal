34767d5c26f5b5862b8cc8140bfa0a9669f2d977
iuryroque <iury.roque@hotmail.com>
2021-12-06 14:37:40 -0300

    Adiciona arquivos ao projeto

---
+++ data.js
@@ -0,0 +1,69 @@
+export default [
+  {
+    title:
+      "01 - PB A canção de Natal",
+    artist: "Nome do cantor",
+    cover: "Arte.jpeg",
+    file: "01- PB A canção de Natal.mp3",
+  },
+  {
+    title: "02 - Dramatização Oh, vem Emanuel",
+    artist: "Nome do cantor",
+    cover: "Arte.jpeg",
+    file: "02 - Dramatização Oh, vem Emanuel.mp3",
+  },
+  {
+    title: "02 - PB Oh, vem Emanuel!",
+    artist: "Nome do cantor",
+    cover: "Arte.jpeg",
+    file: "02 - PB Oh, vem Emanuel!.mp3",
+  },
+  {
+    title: "03- PB Meu Filho e Salvador",
+    artist: "Nome do cantor",
+    cover: "Arte.jpeg",
+    file: "03- PB Meu Filho e Salvador.mp3",
+  },
+  {
+    title: "04 - Dramatização Não há lugar",
+    artist: "Nome do cantor",
+    cover: "Arte.jpeg",
+    file: "04 - Dramatização Não há lugar.mp3",
+  },
+  {
+    title: "04 - PB Não há lugar",
+    artist: "Polly",
+    cover: "Arte.jpeg",
+    file: "04 - PB Não há lugar.mp3",
+  },
+  {
+    title: "05 - PB Que Rei é este",
+    artist: "Nome do cantor",
+    cover: "Arte.jpeg",
+    file: "05 - PB Que Rei é este.mp3",
+  },
+  {
+    title: "06 - Gloria in Excelsis Deo",
+    artist: "Nome do cantor",
+    cover: "Arte.jpeg",
+    file: "06 - Gloria in Excelsis Deo.mp3",
+  },
+  {
+    title: "07 - PB Por Amor",
+    artist: "Nome do cantor",
+    cover: "Arte.jpeg",
+    file: "07 - PB Por Amor.mp3",
+  },
+  {
+    title: "08 - PB Filho do Bom Deus",
+    artist: "Nome do cantor",
+    cover: "Arte.jpeg",
+    file: "08 - PB Filho do Bom Deus.mp3",
+  },
+  {
+    title: "09 - PB Alegria, alegria",
+    artist: "Nome do cantor",
+    cover: "Arte.jpeg",
+    file: "09 - PB Alegria, alegria.mp3",
+  },
+];
